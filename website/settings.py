ce8e3887c782ed49e8a8c474b46e2b5913565d50
nicholaswong112 <nicholaswong@utexas.edu>
2022-09-28 15:21:40 -0500

    first attempt to deploy to render

--- website/settings.py
+++ website/settings.py
@@ -16,22 +16,17 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nhz(a1v$7=bw@j6xk5905&38rr9zjkggm4-7icpx4*mb$1z(5#'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+# SECURITY-SENSITIVE SETTINGS
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-nhz(a1v$7=bw@j6xk5905&38rr9zjkggm4-7icpx4*mb$1z(5#')
+DEBUG = 'RENDER' not in os.environ
 ALLOWED_HOSTS = []
-
+if 'RENDER_EXTERNAL_HOSTNAME' in os.environ:
+    ALLOWED_HOSTS.append(os.environ['RENDER_EXTERNAL_HOSTNAME'])
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,9 +38,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -122,6 +118,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+# For WhiteNoise, for *both* dev and prod (see 'runserver_nonstatic' middleware)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
