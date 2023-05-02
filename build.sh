6243305d6df365e59e93cc5c1d1d3e3bbb6b48e0
Nicholas Wong <nicholaswong@utexas.edu>
2023-05-01 20:05:36 -0500

    migrate before createsuperuser

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ pip install -r requirements.txt
 npm install
 npm run build
 
+python manage.py migrate
 python manage.py createsuperuser --no-input || true
 python manage.py collectstatic --no-input
-python manage.py migrate
